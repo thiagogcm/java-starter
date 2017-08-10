a95f151024858959e5ef92f23e5db8c0b66c8865
Thiago Moura <thiagogcm@gmail.com>
2017-08-10 16:44:18 -0300

    Updating libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath("org.junit.platform:junit-platform-gradle-plugin:1.0.0-M4")
+        classpath("org.junit.platform:junit-platform-gradle-plugin:1.0.0-RC2")
         classpath("com.github.jengelman.gradle.plugins:shadow:2.0.1")
     }
 }
@@ -13,7 +13,6 @@ buildscript {
 plugins {
     application
     checkstyle
-    idea
     jacoco
 }
 
@@ -27,7 +26,7 @@ repositories {
 }
 
 checkstyle {
-    toolVersion = "8.0"
+    toolVersion = "8.1"
 }
 
 jacoco {
@@ -36,17 +35,17 @@ jacoco {
 
 dependencies {
     // Utils libraries
-    //compile("com.google.guava:guava:22.0")
-    //compile("org.apache.commons:commons-lang3:3.5")
-    //compile("com.fasterxml.jackson.core:jackson-core:2.8.8")
-    //compile("com.fasterxml.jackson.core:jackson-databind:2.8.8")
-    //compile("com.fasterxml.jackson.module:jackson-module-parameter-names:2.8.8")
-    //compile("com.fasterxml.jackson.datatype:jackson-datatype-jdk8:2.8.8")
-    //compile("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.8.8")
+    compile("com.google.guava:guava:23.0")
+    compile("org.apache.commons:commons-lang3:3.6")
+    compile("com.fasterxml.jackson.core:jackson-core:2.9.0")
+    compile("com.fasterxml.jackson.core:jackson-databind:2.9.0")
+    compile("com.fasterxml.jackson.module:jackson-module-parameter-names:2.9.0")
+    compile("com.fasterxml.jackson.datatype:jackson-datatype-jdk8:2.9.0")
+    compile("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.9.0")
 
     // Jdbc
-    //implementation("com.h2database:h2:1.4.195") // Driver
-    //compile("com.zaxxer:HikariCP:2.6.2") // Connection pool
+    implementation("com.h2database:h2:1.4.196") // Driver
+    compile("com.zaxxer:HikariCP:2.6.3")         // Connection pool
 
     // Logging libraries
     compile("org.apache.logging.log4j:log4j-core:2.8.2")
@@ -54,16 +53,11 @@ dependencies {
     compile("org.apache.logging.log4j:log4j-slf4j-impl:2.8.2")
 
     // Testing libraries
-    testCompile("org.mockito:mockito-core:2.8.9")
+    testCompile("org.mockito:mockito-core:2.8.47")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.0.0-M4")
-    //testImplementation("org.junit.jupiter:junit-jupiter-params:5.0.0-M4")
-    //testImplementation("org.junit.platform:junit-platform-runner:1.0.0-M4")
-
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.0.0-M4")
-    // To run JUnit4 tests:
-    //testCompile("junit:junit:4.12")
-    //testRuntimeOnly("org.junit.vintage:junit-vintage-engine:4.12.0-M4")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.0.0-RC2")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.0.0-RC2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.0.0-RC2")
 }
 
 application {
@@ -94,6 +88,6 @@ tasks {
     }
 
     "wrapper"(Wrapper::class) {
-        gradleVersion = "4.0"
+        gradleVersion = "4.1"
     }
 }
