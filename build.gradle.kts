a8197b4e45c94b7b9dedaa68a36d88d4eac42ff4
Thiago Moura <thiagogcm@gmail.com>
2018-04-29 17:38:49 -0300

    Update deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,7 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath("org.junit.platform:junit-platform-gradle-plugin:1.0.0")
-        classpath("com.github.jengelman.gradle.plugins:shadow:2.0.1")
+        classpath("com.github.jengelman.gradle.plugins:shadow:2.0.3")
     }
 }
 
@@ -18,7 +17,6 @@ plugins {
 
 apply {
     plugin("com.github.johnrengelman.shadow")
-    plugin("org.junit.platform.gradle.plugin")
 }
 
 repositories {
@@ -26,41 +24,38 @@ repositories {
 }
 
 checkstyle {
-    toolVersion = "8.2"
+    toolVersion = "8.9"
 }
 
 jacoco {
     val run : JavaExec by tasks
 
-    toolVersion = "0.7.9"
+    toolVersion = "0.8.1"
     applyTo(run)
 }
 
 dependencies {
     // Utils libraries
-    //implementation("com.google.guava:guava:23.0")
-    //implementation("org.apache.commons:commons-lang3:3.6")
-    //implementation("com.fasterxml.jackson.core:jackson-core:2.9.1")
-    //implementation("com.fasterxml.jackson.core:jackson-databind:2.9.1")
-    //implementation("com.fasterxml.jackson.module:jackson-module-parameter-names:2.9.1")
-    //implementation("com.fasterxml.jackson.datatype:jackson-datatype-jdk8:2.9.1")
-    //implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.9.1")
+    //implementation("com.google.guava:guava:24.1-jre")
+    //implementation("org.apache.commons:commons-lang3:3.7")
+    //implementation("com.fasterxml.jackson.core:jackson-databind:2.9.5")
+    //implementation("com.fasterxml.jackson.module:jackson-module-parameter-names:2.9.5")
+    //implementation("com.fasterxml.jackson.datatype:jackson-datatype-jdk8:2.9.5")
+    //implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.9.5")
 
     // Jdbc
     //implementation("com.h2database:h2:1.4.196")
-    //implementation("com.zaxxer:HikariCP:2.7.1")
+    //implementation("com.zaxxer:HikariCP:2.7.8")
 
     // Logging libraries
-    implementation("org.apache.logging.log4j:log4j-core:2.9.0")
-    implementation("org.apache.logging.log4j:log4j-api:2.9.0")
-    implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.9.0")
+    implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.11.0")
 
     // Testing libraries
-    testCompile("org.mockito:mockito-core:2.10.0")
+    testCompile("org.mockito:mockito-core:2.18.3")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.0.0")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.0.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.0.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.1.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.1.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.1.1")
 }
 
 application {
@@ -97,29 +92,14 @@ tasks {
         sourceSets(java.sourceSets["main"])
     }
 
+    "jacocoTestReport"(JacocoReport::class) {
+        reports {
+            xml.isEnabled = true
+            html.isEnabled = true
+        }
+    }
+
     "wrapper"(Wrapper::class) {
-        gradleVersion = "4.2"
-    }
-}
-
- tasks.withType<JacocoReport> {
-    reports {
-        xml.isEnabled = true
-        html.isEnabled = true
-    }
-}
-
-afterEvaluate {
-    val junitPlatformTest : JavaExec by tasks
-    
-    jacoco {
-        applyTo(junitPlatformTest)
-    }
-
-    task<JacocoReport>("jacocoJunit5TestReport") {
-        executionData(junitPlatformTest)
-        sourceSets(java.sourceSets["main"])
-        sourceDirectories = files(java.sourceSets["main"].allSource.srcDirs)
-        classDirectories = files(java.sourceSets["main"].output)
+        gradleVersion = "4.7"
     }
 }
